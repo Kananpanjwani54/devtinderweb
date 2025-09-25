8e28538d8ad1878d51b822319653a78e34bbfddc
kananpanjwani682gmail.com <kananpanjwani68@gmail.com>
2025-09-25 11:44:13 +0530

    updated:ProfilePic will be shown

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,41 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addFeed } from '../utils/feedSlice';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import SwipeCard from '../components/userCard';
+
+
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  // const navigate=useNavigate();
+
+  const getFeed = async () => {
+    if (feed) return; // Prevents unnecessary API calls if data already exists
+
+    try {
+      const res = await axios.get(BASE_URL + '/user/feed', { withCredentials: true });
+      console.log(res);
+      dispatch(addFeed(res.data)); // Dispatch action to add feed to Redux store
+    } catch (err) {
+      console.log(err);
+      // TODO: handle error
+      
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
   return (
-    <div>Feed</div>
+  feed &&(
+    <div>
+    <SwipeCard user={feed[0]}/>
+    </div>
   )
-}
+  );
+};
 
 export default Feed;
