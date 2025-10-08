7eb29a6aa1b2e1ef4fcee9d4e037e13c8d42de25
kananpanjwani682gmail.com <kananpanjwani68@gmail.com>
2025-10-08 07:46:38 +0530

    url fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 
 
-// export const BASE_URL=location.hostname ==="localhost" ? "http://localhost:3000": "/api"
+export const BASE_URL=location.hostname ==="localhost" ? "http://localhost:3000": "/api"
 
 // // export const BASE_URL="/api"
-//link for render
-export const BASE_URL="https://devtinder-j4uj.onrender.com"||"/api"
